caf985c5899848b74c5a7264e62e8e92533d4c04
AndrewMorrow <beardedmongo@gmail.com>
2020-10-12 18:36:21 -0500

    adds search form to html

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const searchFieldElem = $(".search-bar");
 const searchHistoryElem = $(".search-history");
 const cityDisplayElem = $(".city-display");
 const forecastElem = $(".forecast");
+
+// var userBrowserLocation =
