66dcbf7212a6c75a18fc1bf980bb9619af0aebb1
Andrew Morrow <beardedmongo@gmail.com>
2020-10-14 00:32:26 -0500

    adds forecast function

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ var temperature;
 var humidity;
 var windSpeed;
 var userCityName;
+var apiBase = `http://api.openweathermap.org/data/2.5/`;
+var date = Date();
+
+console.log(date);
 
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(setLocationGeo);
@@ -21,19 +25,21 @@ function setLocationGeo(position) {
     let longitude = position.coords.longitude;
 
     // takes lat and long and passes to this function
-    let apiGeo = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&APPID=${apiKey}`;
+    let apiGeo = `${apiBase}weather?lat=${latitude}&lon=${longitude}&units=imperial&APPID=${apiKey}`;
 
     $.ajax(apiGeo, {
         success: function (data) {
             var weatherReceived = data;
-            console.log(weatherReceived);
+            // console.log(weatherReceived);
             cityNameReceived = weatherReceived.name;
             weatherIconId = weatherReceived.weather[0].icon;
             temperature = Math.round(weatherReceived.main.temp);
             humidity = weatherReceived.main.humidity;
             windSpeed = weatherReceived.wind.speed;
 
-            var cityNameHeader = $(`<h2> ${cityNameReceived} </h2>`);
+            var cityNameHeader = $(
+                `<h2> ${cityNameReceived} <img src=" http://openweathermap.org/img/wn/${weatherIconId}.png"></img></h2>`
+            );
             var tempDisplay = $(`<p> Temperature: ${temperature} °F </p>`);
             var humDisplay = $(`<p> Humidity: ${humidity} % </p>`);
             var windSpeedDisplay = $(`<p> Wind Speed: ${windSpeed} MPH </p>`);
@@ -46,16 +52,9 @@ function setLocationGeo(position) {
             $(errorElem).text("An Error occured while retrieving data");
         },
     });
-    let apiUvi = `http://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&units=imperial&APPID=${apiKey}`;
-    $.ajax(apiGeo, {
-        success: function (data) {
-            var uviReceived = data;
-            console.log(uviReceived);
-        },
-        error: function () {
-            $(errorElem).text("An Error occured while retrieving data");
-        },
-    });
+
+    getUvi(latitude, longitude);
+    getForecast(latitude, longitude);
 }
 
 // // recieves weather from api with users browser geolocation
@@ -72,22 +71,65 @@ searchButtonElem.on("click", (e) => {
 });
 
 function recieveUserCity(cityCall) {
-    let apiUserCity = `http://api.openweathermap.org/data/2.5/weather?q=${cityCall}&units=imperial&APPID=${apiKey}`;
+    let apiUserCity = `${apiBase}weather?q=${cityCall}&units=imperial&APPID=${apiKey}`;
 
     $.ajax(apiUserCity, {
         success: function (data) {
             var weatherReceived = data;
             console.log(weatherReceived);
+            cityNameReceived = weatherReceived.name;
             weatherIconId = weatherReceived.weather[0].icon;
             temperature = weatherReceived.main.temp;
             humidity = weatherReceived.main.humidity;
             windSpeed = weatherReceived.wind.speed;
             var savedCities = localStorage.setItem("City", cityCall);
+            let latitude = weatherReceived.coord.lat;
+            let longitude = weatherReceived.coord.lon;
+
+            var cityNameHeader = $(
+                `<h2> ${cityNameReceived} <img src=" http://openweathermap.org/img/wn/${weatherIconId}.png"></img> </h2>`
+            );
+            var tempDisplay = $(`<p> Temperature: ${temperature} °F </p>`);
+            var humDisplay = $(`<p> Humidity: ${humidity} % </p>`);
+            var windSpeedDisplay = $(`<p> Wind Speed: ${windSpeed} MPH </p>`);
+            cityDisplayElem.append(cityNameHeader);
+            cityDisplayElem.append(tempDisplay);
+            cityDisplayElem.append(humDisplay);
+            cityDisplayElem.append(windSpeedDisplay);
+            getUvi(latitude, longitude);
         },
         error: function () {
             $(errorElem).text(
                 "An Error occured while retrieving data. Please enter the full city name and the full state name."
             );
+        },
+    });
+}
+
+function getUvi(lat, long) {
+    let apiUvi = `${apiBase}uvi?lat=${lat}&lon=${long}&units=imperial&APPID=${apiKey}`;
+    $.ajax(apiUvi, {
+        success: function (data) {
+            var uviReceived = data.value;
+            var uviDisplay = $(`<p> UV Index: ${uviReceived} </p>`);
+            cityDisplayElem.append(uviDisplay);
+            // console.log(uviReceived);
+        },
+        error: function () {
+            $(errorElem).text("An Error occured while retrieving data");
+        },
+    });
+}
+
+function getForecast(lat, long) {
+    let apiForecast = `${apiBase}forecast?lat=${lat}&lon=${long}&units=imperial&APPID=${apiKey}`;
+    $.ajax(apiForecast, {
+        success: function (data) {
+            var forecastReceived = data.value;
+            console.log(forecastReceived);
+        },
+        error: function () {
+            $(errorElem).text("An Error occured while retrieving data");
         },
     });
 }
